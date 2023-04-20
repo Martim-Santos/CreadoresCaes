8209689b8ff6aa59f06be9758e1942770fb06ac0
Martim Santos <aluno24235@ipt.pt>
2023-04-20 13:02:36 +0100

    Anotações

--- CriadoresCaes/CriadoresCaes/Models/Criadores.cs
+++ CriadoresCaes/CriadoresCaes/Models/Criadores.cs
@@ -1,4 +1,7 @@
-﻿namespace CriadoresCaes.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CriadoresCaes.Models {
     /// <summary>
     /// dados dos criadores dos animais
     /// </summary>
@@ -11,38 +14,49 @@
             ListaRacas = new HashSet<Racas>();
         }
 
+        /*
+         */
 
         public int Id { get; set; }
 
         /// <summary>
         /// nome do criador
         /// </summary>
+        [Required(ErrorMessage ="O {0} é de preenchismento obrigratório.")]
         public string Nome { get; set; }
 
         /// <summary>
         /// nome pelo qual o criador é conhecido
         /// no mercado de venda de animais
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchismento obrigratório.")]
+        [Display(Name ="Nome Comercial")]
         public string NomeComercial { get; set; }
 
         /// <summary>
         /// morada do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchismento obrigratório.")]
         public string Morada { get; set; }
 
         /// <summary>
         /// Código Postal
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchismento obrigratório.")]
+        [Display(Name ="Código Postal")]
         public string CodPostal { get; set; }
 
         /// <summary>
         /// Email do criador
         /// </summary>
+        [EmailAddress][Required(ErrorMessage = "O {0} é de preenchismento obrigratório.")]
         public string Email { get; set; }
 
         /// <summary>
         /// Telemóvel do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchismento obrigratório.")]
+        [Display(Name ="Telemóvel")]
         public string Telemovel { get; set; }
 
         /*+++++++++++++++++++++++++++++++++++++++++++
